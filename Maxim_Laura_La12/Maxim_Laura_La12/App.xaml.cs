88ebf30d4a099e69795c39048b077e0dcc8f4026
Laura Daniela Maxim <Maxim.Da.Laura@student.utcluj.ro>
2022-01-06 15:46:57 +0200

    Add project files.

---
+++ Maxim_Laura_La12/Maxim_Laura_La12/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Maxim_Laura_La12.Data;
+
+namespace Maxim_Laura_La12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            InitializeComponent();
+
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Maxim_Laura_La12/Maxim_Laura_La12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Maxim_Laura_La12.Models;
+
+namespace Maxim_Laura_La12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
